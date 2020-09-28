d79f6bab58cb0057e08b6c22f4af17aa2b729e4f
pratyaksha sharma <53622346+pratyaksha10@users.noreply.github.com>
2020-09-28 09:54:49 +0530

    Update reservation.c

--- reservation.c
+++ reservation.c
@@ -115,3 +115,177 @@ void get_trains(char from[14], char to[14])
           main_exit();
       }
 }
+//HASH TABLE TO STORE PASSENGER'S DETAILS
+#define TABLE_SIZE 100
+
+struct person
+{
+    int pnr;
+    int age;
+    char name[20];
+    char sex[1];
+    long long int phone_num;
+    float bill;
+    char train_name[30];
+    int train_id;
+    char source[20];
+    char destination[20];
+
+};
+
+
+struct person *hash_table[TABLE_SIZE];//array of pointers
+int pnr=3302;
+
+//Quadratic probing in case of collision.
+int hash(int p)
+{
+    int hash_value=p%TABLE_SIZE;
+    return hash_value;
+}
+
+void get_details(int train_num, char from[14],char to[14])
+{
+    int seats,i;
+    int index;
+
+      struct node *ptr,*q;
+      ptr=root;
+
+      while(ptr!=NULL)
+      {
+        if(train_num==ptr->train_id)
+        {
+            q=ptr;
+        }
+        ptr=ptr->link;
+      }
+
+    seats: printf("\nEnter number of seats: ");
+    scanf("%d",&seats);
+
+    if(seats<0)
+    {
+        printf("\nNUMBER OF SEATS CANNOT BE NEGATIVE \n");
+        getch();
+        goto seats;
+    }
+
+    if(seats>6)
+    {
+        printf("\nNUMBER OF SEATS CANNOT EXCEED 6\n");
+        getch();
+        goto seats;
+    }
+
+    update_seats(train_num,seats);
+
+
+
+    struct person *p;
+
+    for(i=0; i<seats;i++)
+    {
+        system("cls");
+        printf("\n------------RESERVATION DETAILS---------------\n\n");
+        printf("\nDetails of passenger %d\n\n", i+1);
+        p=(struct person*)malloc(sizeof(struct person));
+        index=hash(pnr);
+
+        hash_table[index]=p;
+        p->pnr=pnr;
+
+        char temp; char temp1; //buffer , null character is stored at the start of string when integer value is entered
+
+        scanf("%c",&temp);
+
+        printf("Enter Full Name: ");
+        scanf("%[^\n]", p->name);
+
+        gender: printf("Enter Gender M, F or O: ");
+        scanf("%s",&p->sex);
+
+        if(!(strcmp(p->sex,"F")==0||strcmp(p->sex,"M")==0||strcmp(p->sex,"O")==0))
+        {
+            printf("\nINVALID ENTRY\n");
+            goto gender;
+        }
+
+        age: printf("Enter Age: ");
+        scanf("%d",&p->age);
+
+        if(p->age<0)
+        {
+        printf("\nAGE CANNOT BE NEGATIVE\n");
+        getch();
+        goto age;
+        }
+
+        else if(p->age>120)
+        {
+        printf("\nINVALID AGE\n");
+        getch();
+        goto age;
+        }
+
+        phone_num: printf("ENTER 10 DIGIT PHONE NUMBER: ");
+        scanf("%lld",&p->phone_num);
+
+
+
+        if(p->phone_num<1000000000)
+        {
+            printf("\nINVALID PHONE NUMBER\n");
+            goto phone_num;
+        }
+
+        if(p->phone_num>9999999999)
+        {
+            printf("\nINVALID PHONE NUMBER\n");
+            goto phone_num;
+        }
+
+        strcpy(p->source,from);
+        strcpy(p->destination,to);
+        strcpy(p->train_name,q->train_name);
+        p->train_id=q->train_id;
+        p->bill=q->cost;
+        pnr++;
+        printf("\nYOUR PNR NUMBER IS:%d", p->pnr);
+        getch();
+    }
+
+    printf("\n\n\nRESERVATION SUCCESSFUL!");
+
+    main_exit();
+}
+
+
+void print_table()
+{
+system("cls");
+int i;
+int count=0;
+for(i=0; i<TABLE_SIZE; i++)
+{
+        if(hash_table[i] != NULL)
+        {
+        printf("%d\t",hash_table[i]->pnr);
+        printf("%s\t",hash_table[i]->name);
+        printf("%d\t",hash_table[i]->age);
+        printf("%s\t",hash_table[i]->sex);
+        printf("%d\t",hash_table[i]->phone_num);
+        printf("\n");
+        }
+        else
+        {
+        count++;
+        printf("");
+        }
+}
+
+if(count==TABLE_SIZE)
+{
+    printf("\n\n\t\tNO PASSENGERS AS YET!!\n\n");
+    main_exit();
+}
